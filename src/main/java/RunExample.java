b5ff69c98de4066331c6154b9c2997b99837e799
Markus Göckeritz <markus@goeckeritz.ch>
2018-03-20 10:42:51 +0100

    added kave dependencies and copied the getting started example from the kave examples github repo

---
+++ src/main/java/RunExample.java
@@ -0,0 +1,25 @@
+import examples.GettingStarted;
+
+public class RunExample {
+
+
+
+    /*
+     * download the interaction data and unzip it into the root of this project (at
+     * the level of the pom.xml). Unpack it, you should now have a folder that
+     * includes a bunch of folders that have dates as names and that contain .zip
+     * files.
+     */
+    public static String eventsDir = "Events-170301-2";
+
+    /*
+     * download the context data and follow the same instructions as before.
+     */
+    public static String contextsDir = "Contexts-170503";
+    
+    public static void main(String[] args) {
+        GettingStarted gs = new GettingStarted(eventsDir);
+        gs.run();
+    }
+    
+}
