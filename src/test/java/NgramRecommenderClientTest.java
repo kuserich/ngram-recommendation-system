674e14b61d34f7a9d8dbc28908817959b6126f10
Julian Iff <julian@seerow.ch>
2018-05-31 23:17:19 +0200

    Added new "mock" "data" "" , to test assertequal of ngram output

--- src/test/java/NgramRecommenderClientTest.java
+++ src/test/java/NgramRecommenderClientTest.java
@@ -1,7 +1,13 @@
+import cc.kave.commons.model.naming.codeelements.IMethodName;
+import cc.kave.rsse.calls.datastructures.Tuple;
 import ngram.NgramRecommenderClient;
+import opennlp.tools.util.StringList;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
 
 public class NgramRecommenderClientTest {
     private static String outputDir = "MockData/output/Microsoft.Win32.txt";
@@ -9,7 +15,12 @@ public class NgramRecommenderClientTest {
     @Test
     public void NgramRecommender () throws IOException {
 
-        NgramRecommenderClient nrc = new NgramRecommenderClient();
-        nrc.train(outputDir);
+        NgramRecommenderClient nrc = new NgramRecommenderClient("MockData/models/NUnit.Framework.xml");
+        Set<Tuple<IMethodName, Double>> modeloutput = nrc.query(
+                new StringList(
+                        "NUnit.Framework.Assert,AreEqual",
+                        "NUnit.Framework.Assert,AreEqual"
+                ));
+        assertEquals(modeloutput.toString(), "[<<NUnit.Framework.Assert,AreSame>, 0.2857142857142857>]");
     }
 }
