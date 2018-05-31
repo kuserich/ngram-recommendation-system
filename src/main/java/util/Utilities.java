29cd81d4105a477021c05034353d848a61f20890
Markus Göckeritz <markus@goeckeritz.ch>
2018-05-31 20:45:35 +0200

    added comments to methods in utilities

--- src/main/java/util/Utilities.java
+++ src/main/java/util/Utilities.java
@@ -31,18 +31,53 @@ public class Utilities {
         // we check both the name of the declaring type as well as the namespace
         return methodName.getDeclaringType().getName().equals("???")
                 || methodName.getDeclaringType().getNamespace().getIdentifier().equals("???")
-                || methodName.getDeclaringType().getNamespace().getIdentifier().contains("`1[[");
+                || methodName.getDeclaringType().getNamespace().getIdentifier().contains("[[");
     }
-    
+
+    /**
+     * Transforms the given list of {@link APIToken} to a {@link StringList}
+     * containing the string representation {@link APIToken#toString()} of
+     * all tokens.
+     * 
+     * We use this for the {@link ngram.NgramRecommenderClient}.
+     * 
+     * @see evaluation.NgramRecommenderEvaluation#testWithModel(String, APIToken, List)
+     *          uses this method
+     * 
+     * @param sentence
+     *          list of APITokens
+     *          
+     * @return
+     *          StringList containing all APITokens as strings
+     */
     public static StringList apiSentenceToStringList(List<APIToken> sentence) {
         String[] strings = new String[sentence.size()];
         for(int i=0;i<sentence.size();i++) {
             APIToken token = sentence.get(i);
-            strings[i] = token.getType()+","+token.getOperation();
+            strings[i] = token.toString();
         }
         return new StringList(strings);
     }
-    
+
+    /**
+     * Returns a capitalized form of the given string.
+     * That is, changes the case of the first character in the given string to
+     * uppercase.
+     * 
+     * Example:
+     *      "input" -> "Input"
+     *     
+     * @see APIToken#getType()
+     *          uses this method. We noticed that the method name in a
+     *          type of an APIToken sometimes is lower case and if we 
+     *          use it with the namespace, it should be capitalized
+     * 
+     * @param str
+     *          string that is capitalized
+     *          
+     * @return
+     *          capitalized string
+     */
     public static String capitalize(String str) {
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
